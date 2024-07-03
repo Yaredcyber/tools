37838024fe9ee66542a150768c555243c3b9192d
Yared <147349965+Yaredcyber@users.noreply.github.com>
2024-07-03 10:35:12 -0300

    Update install.sh

--- zipper/install.sh
+++ zipper/install.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+
 echo "Requirement installation"
 sudo apt update
 sudo apt install -y python3-pip
 pip3 install pyzipper
-pip install platform 
+pip install platform
 pip install zipfile
 echo "Installation complete"
+git clone https://github.com/Yaredcyber/tools.git
+cd tools
+mv zipper ../
+cd ..
+rm -rf tools
+echo "Installation complete"
+
